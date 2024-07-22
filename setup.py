5c4674935fa6c0a10ffee69da3af6a27bd53b1ef
leroy-mataa <leroy.ab@umbaji.org>
2024-07-22 15:43:14 +0000

    test: change logic

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="yodi_demo",
-    version="0.0.0",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=[
         # Add dependencies here.
