54b241b0cbc3a2f3bfc137b4ee89fcf14123895c
leroy-mataa <leroy.ab@umbaji.org>
2024-07-23 02:37:52 +0000

    test: update code logic

--- setup.py
+++ setup.py
@@ -5,12 +5,17 @@ with open("README.md", "r") as f:
     description = f.read()
 
 setup(
-    name="yodi_py",
-    version="0.3.0",
+    name="yodi-umbaji",
+    version="0.5.0",
     packages=find_packages(),
     install_requires=[
-        # Add dependencies here.
-        # e.g. 'numpy>=1.11.1'
+        'pathlib',
+        'tensorflow',
+        'matplotlib',
+        'numpy',
+        'seaborn',
+        'IPython',
+        'gradio',
     ],
 
     long_description=description,
---
+++ yodi-umbaji/Yodi.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9d5e4529cede2b0e1848d018bf4adda382b10cd2f1063b50d50588e30419997b
+size 4477
